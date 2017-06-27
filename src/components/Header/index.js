4ab80257827ed0d1de363c7547ffee79958cef39
Miguel Cervantes <m_cervantes5@u.pacific.edu>
2017-06-26 20:19:31 -0700

    Created a mockup of final design

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,7 +3,16 @@ import React, { Component } from 'react';
 class Header extends Component {
 	render(){
 		return(
-			<div>Header</div>	
+			<div className="header-container">
+				<div className="header-brand-container">
+					<div className="header-brand-text">
+						<span>EtherSpin</span>
+					</div>
+					<div className="header-brand-logo">
+						<img src="https://upload.wikimedia.org/wikipedia/commons/8/8a/TheStructorr_Lamborghini_Gallardo.svg"/>
+					</div>
+				</div>
+			</div>
 		);
 	}
 }
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,7 +3,56 @@ import React, { Component } from 'react';
 class Home extends Component {
 	render(){
 		return(
-			<div>Etas;ldkfjas;df</div>	
+			<div className="content-container">
+				<div className="content-top">
+					<div className="container-fluid">
+						<div className="row">
+							<div className="col-xs-8 col-sm-8 col-md-8">
+								<div className="container-fluid">
+									<div className="row">
+										<div className="col-xs-5 col-sm-5 col-md-5">
+											<div className="spinner"></div>
+										</div>
+										<div className="col-xs-7 col-sm-7 col-md-7">
+											<div className="">
+												Number Spun/Landed On:
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div className="col-xs-4 col-sm-4 col-md-4">
+								<div className="latest-bets-container">
+									<div className="latest-bets-header">
+										<span>Latest Bets</span>
+									</div>
+									<div className="latest-bets-timeline">
+										<span>timeline with bets</span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="content-bottom">
+					<div className="container-fluid">
+						<div className="row">
+							<div className="col-xs-8 col-sm-8 col-md-8">
+								<div className="board-container">
+									board
+								</div>
+							</div>
+							<div className="col-xs-4 col-sm-4 col-md-4">
+								<div className="place-bets-container">
+									<div className="place-bets-timeline">
+										place bets stuff
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>	
 		);
 	}
 }
